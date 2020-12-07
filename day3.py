b9d4a15c34c953ebc97be9252f016a133841b94b
etarakoem <50989367+etarakoem@users.noreply.github.com>
2020-12-07 15:57:52 -0500

    Uploaded wrong one

--- day3.py
+++ day3.py
@@ -1,62 +1,68 @@
-# file = 'day3.txt'
-file = 'scrap.txt'
+file = 'day3.txt'
+# file = 'scrap.txt'
 f = open(file,'r')
 lists = f.read().splitlines()
-print(lists)
+# Get individual list of input:
+wire_1 = lists[0].split(',')
+wire_2 = lists[1].split(',')
+# print('wire 1:',wire_1)
+# print('wire 2:',wire_2)
 
-# Get cordinate of maximum up, and right
-def final_vertex(array):
-    result = []
-    x_sum_r = 0
-    x_sum_l = 0
-    y_sum_u = 0
-    y_sum_d = 0
-    for string in array:
-        parts = string.split(',')
-        print(parts)
-        cordinate = [int(i[1:]) for i in parts ]
-        x = []
-        y = []
-        print('Cordinate: ',cordinate)
-        for i in range(len(cordinate)):
-            if parts[i][0] == 'L':
-                x.append(-1*cordinate[i])
-                x_sum_l += cordinate[i]
-            elif parts[i][0] == 'R':
-                x.append(cordinate[i])
-                x_sum_r += cordinate[i]
-            if parts[i][0] == 'D':
-                y.append(-1*cordinate[i])
-                y_sum_d += cordinate[i]
-            elif parts[i][0] == 'U':
-                y.append(cordinate[i])
-                y_sum_u += cordinate[i]
-        print('Cordinate after: ', (x,y))
-        result.append((x,y))
-    return result,(-1*x_sum_l,x_sum_r),(-1*y_sum_d,y_sum_u)
-# print(final_vertex(lists))
-# mapping,extend_
-mapping,h,v =final_vertex(lists)
-print('horizontal:', h)
-print('vertical:',v)
+def where_tf_am_i(where_to):
+    # I will make a dictionary, to take note of every single steps
+    # Then look for each wires if they crossed at any similar cordinate
+    # As known as, intersection
+    all_turn = {}
 
-def print_matrix(matrix):
-    for i in range(len(matrix)):
-        print(matrix[i])
-def replace_at_index(matrix,row,position,char):
-    string = matrix[row]
-    return string[:position] + char + string[position+1:]
-def mapping_generate(l,r,u,d,startpoint = [-1,0]):
-    matrix = []
-    startpoint = [startpoint[0]-1,startpoint[1]+1]
-    col = 0
-    col = abs(l) + abs(r)+2
-    tmp = '.' * col
-    for row in range(abs(u)+abs(d)+2):
-        matrix.append(tmp)
-        # print(matrix[row])
-    matrix[startpoint[0]] = replace_at_index(matrix,startpoint[0],startpoint[1],'O')
-    print_matrix(matrix)
-    pass
+    # x,y cordinate to take note at every single point of steps
+    x_pos = 0
+    y_pos = 0
+    # Count up every turn, to do the latter part in part 2, count steps
+    turn = 0
 
-mapping_generate(h[0],h[1],v[1],v[0])
+    for pos in where_to:
+        # Get up,down,left,right, the direction
+        heading = pos[0]
+
+        # Get the value of steps needed
+        steps = int(pos[1:])
+
+        # Number of steps needed
+        go_x = 0
+        go_y = 0
+        if heading == 'R':
+            go_x = 1
+        elif heading == 'L':
+            go_x = -1
+        elif heading == 'D':
+            go_y = -1
+        elif heading == 'U':
+            go_y = 1
+        for i in range(steps):
+            x_pos += go_x
+            y_pos += go_y
+            turn += 1
+            if (x_pos,y_pos) not in all_turn:
+                all_turn[(x_pos,y_pos)] = turn
+    return all_turn
+
+# Turn off the comment to see all of the map.
+wire_1_map = where_tf_am_i(wire_1)
+wire_2_map = where_tf_am_i(wire_2)
+# print('Wire_1 map:',wire_1_map)
+# print('Wire_2 map:',wire_2_map)
+
+# Get intersections:
+intersections = set(wire_1_map).intersection(wire_2_map)
+# print('intersections: ',intersections)
+
+sum_cordinate = lambda x: abs(x[0])+ abs(x[1])
+
+# Part 1 first answer, get all the distance
+intersections_sum = list(map(sum_cordinate,intersections))
+print('Final result part 1: ',min(intersections_sum))
+
+# Part 2:
+steps = [wire_1_map[i] + wire_2_map[i] for i in intersections]
+# print(steps)
+print('Minimum steps at part 2 = ',min(steps))
